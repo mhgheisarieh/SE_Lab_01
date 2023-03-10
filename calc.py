d7c6345b8d8809c66e844ce8b1c8a737f6206bce
mhgheisarieh <mgheysariyeh@gmail.com>
2023-03-10 22:10:48 +0330

    adding 4 main operations
    first enter first operand
    then enter operator
    finaly enter second operand

--- calc.py
+++ calc.py
@@ -1,9 +1,40 @@
+a = 0
+b = 0
+status = 0
+operation = 0
+
 def process_input(input):
-    pass
+    global a, b, status, operation
+    if status == 0:
+        a = int(input)
+        status = 1
+        return
+    if status == 1:
+        if input == "+":
+            operation = 1
+        if input == "-":
+            operation = 2
+        if input == "*":
+            operation = 3
+        if input == "/":
+            operation = 4
+        status = 2
+        return
+    if status == 2:
+        b = int(input)
+        if operation == 1:
+            print(a + b)
+        if operation == 2:
+            print(a - b)
+        if operation == 3:
+            print(a * b)
+        if operation == 4:
+            print(a / b)
+        operation = 0
 
 
 while(True):
-    input = input()
-    if input == 'end':
+    input_ = input()
+    if input_ == 'end':
         break
-    process_input(input)
+    process_input(input_)
